93e1c8faddba6bcc2616e67e41d3b9ebeb4e1fd5
Balaji Masal <85933440+IXBalaji@users.noreply.github.com>
2025-07-08 11:11:41 +0530

    Move Production Trends chart below Production Schedule

--- src/App.tsx
+++ src/App.tsx
@@ -106,8 +106,8 @@ function App() {
             </div>
 
             {/* Main Content Grid */}
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
-              <div className="lg:col-span-2">
+            <div className="space-y-6 mb-6">
+              <div>
                 <ProductionSchedule />
               </div>
               <div>
